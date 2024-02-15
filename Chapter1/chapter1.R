d25d849ce094efcc5492f2660661689bd88cd610
samfraolt <159860452+samfraolt@users.noreply.github.com>
2024-02-14 20:32:19 -0600

    Chapter 1

---
+++ Chapter1/chapter1.R
@@ -0,0 +1,40 @@
+#Chapter 1
+
+1+1
+# This command would print [1]2
+
+#Exercise
+
+#for this exercise I chose to use the number 100
+
+#Addition: Add 2 to the number you chose
+100+2
+##102
+
+#Multiply the result you find by 3
+102*3
+##306
+
+#Subtract 6 from the result you find above
+306-6
+##300
+
+#Divide the result you get by 3
+300/3
+##100
+
+#To see how many numbers are between two values we use :
+1:6
+## 1 2 3 4 5 6
+
+#Create an object named die that contains the numbers from 1 to 6
+die<-1:6
+## 1 2 3 4 5 6
+
+# Now that we've named our function die we can do other operations such as
+die-1
+## 0 1 2 3 4 5
+die/2
+## 0.5 1.0 1.5 2.0 2.5 3.0
+die*die
+## 1 4 9 16 25 36
